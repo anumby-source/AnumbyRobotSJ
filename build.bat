2452ed4381785540cbba0feb662c9542bed0de13
Chris Arnault <chris.arnault.1@gmail.com>
2023-03-28 09:53:56 +0200

    - mettre en place la structure en packages et la position des différents fichiers - setup.py - README.md - AnumbyFormes - AnumbyVehicule

--- build.bat
+++ build.bat
@@ -1,6 +1,10 @@
+set /P VERSION=<VERSION
+echo "version = %VERSION%"
+set PACKAGE=AnumbyRobotSJ
+REM rmdir dist /s/q
 python setup.py sdist bdist_wheel
-twine check dist/*
-tar tzf dist\AnumbyRobotSJ-1.0.0.tar.gz
-twine upload -u chris.arnault --repository-url https://test.pypi.org/legacy/ dist\*
-
-
+twine check dist/%PACKAGE%-%VERSION%-py3-none-any.whl
+tar tzf dist\%PACKAGE%-%VERSION%.tar.gz
+twine upload -u chris.arnault --repository-url https://test.pypi.org/legacy/ dist\%PACKAGE%-%VERSION%*.*
+REM pip install -i https://test.pypi.org/simple/ AnumbyRobotSJ==%VERSION%
+REM twine upload -u chris.arnault dist\%PACKAGE%-%VERSION%*.*
--- setup.py
+++ setup.py
@@ -1,28 +1,50 @@
 import pathlib
-from setuptools import setup
+from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
 README = (HERE/"README.md").read_text()
+VERSION = (HERE/"VERSION").read_text()
 
 setup(
     name = "AnumbyRobotSJ",
-    version = "1.0.0",
+    version = VERSION,
     description = "Software du Robot Anumby pour le Service Jeunesse de Bures sur Yvette ",
     long_description = README,
     long_description_content_type = "text/markdown",
     url = "https://github.com/anumby-source/RobotServiceJeunesse2023/tree/main/MasterMind",
     author = "Chris Arnault",
     author_email = "chris.arnault@gmail.com",
-    license = "MIT License",
+    license = "CeCILL-B",
     classifiers = [
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.9",
+        "License :: CeCILL-B Free Software License Agreement (CECILL-B)",
+        "Programming Language :: Python :: 3",
     ],
-    packages = ["MasterMind"],
+    packages = ["AnumbyMasterMind", "AnumbyFormes", "AnumbyVehicule"],
     include_package_data = True,
-    install_requires = [],
+
+    package_data = {"AnumbyFormes": ["data/*.npy",
+                                     "dataset/*/*.jpg",
+                                     "run/models/*.h5"],
+                    "AnumbyVehicule": ["*.jpg"]
+                    },
+
+    install_requires = [
+        "Pillow == 9.1.0",
+        "numpy >= 1.24.2",
+        "opencv-python >= 4.7.0",
+        "tensorflow >= 2.11.0",
+        "keras >= 2.11.0",
+        "scikit-learn >= 1.2.2",
+        "pandas >= 1.5.3",
+        "matplotlib >= 3.7.1"
+    ],
+
     entry_points = {
-        "console_scripts": ["MasterMind = MasterMind:__main__.main"]
+        "console_scripts": [
+            "AnumbyMasterMind = AnumbyMasterMind:__main__.main",
+            "AnumbyFormes = AnumbyFormes:__main__.main",
+            "AnumbyVehicule = AnumbyVehicule:__main__.main",
+        ]
     },
 )
